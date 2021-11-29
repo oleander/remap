d582ea604393485d1183f633bbdb5481c6dc6b41
Linus Oleander <oleander@users.noreply.github.com>
2021-11-29 19:20:07 +0100

    remove unused examples

--- spec/examples.rb
+++ spec/examples.rb
@@ -1,37 +1,5 @@
 # frozen_string_literal: true
 
-shared_examples Remap do |options = {}|
-  subject { mapper.new(**options).call(input) }
-
-  it { is_expected.to match(output) }
-end
-
-shared_examples Remap::Rule::Map do
-  subject do
-    described_class.call(
-      rule: build(:void),
-      path: { to: to, map: from }
-    ).call(state.result(input))
-  end
-
-  it { is_expected.to include(output) }
-end
-
-shared_examples Remap::Rule::Collection::Empty do
-  it_behaves_like Remap::Rule::Collection do
-    let(:rules) { [] }
-  end
-end
-
-shared_examples Remap::Rule::Collection::Filled do
-  subject { rule.call(new_state) }
-
-  let(:rule) { described_class.call(rules: rules) }
-  let(:new_state) { state.result(input) }
-
-  it { is_expected.to be_a(Remap::State) }
-end
-
 shared_examples "a success" do
   it { is_expected.to have(0).problems }
   it { is_expected.to contain(expected) }
@@ -41,12 +9,4 @@ shared_examples Remap::Base do |options = {}|
   subject { mapper.call(input, **options) }
 
   it { is_expected.to have_attributes(to_hash: include(output).or(include(success: output))) }
-
-  after do |example|
-    if example.metadata[:last_run_status] == "failed"
-      mapper.call(input, **options) do |result|
-        pp result
-      end
-    end
-  end
 end
