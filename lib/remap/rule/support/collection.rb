5cbb37469378915468b50d1d7c57c256686ff2bb
Linus Oleander <oleander@users.noreply.github.com>
2021-11-29 22:17:12 +0100

    add abstract to collection

--- lib/remap/rule/support/collection.rb
+++ lib/remap/rule/support/collection.rb
@@ -3,6 +3,18 @@
 module Remap
   class Rule
     class Collection < Dry::Interface
+      attribute :rules, Array
+
+      # @abstract
+      #
+      # @param state [State]
+      #
+      # @return [State]
+      def call(state)
+        raise NotImplementedError, "#{self.class}#call not implemented"
+      end
+
+      # @return [Proc]
       def to_proc
         method(:call).to_proc
       end
