49bfb0e94b77856dc68647f3636c69b8f47d26af
Linus Oleander <oleander@users.noreply.github.com>
2021-11-29 18:48:43 +0100

    clean up spec helper, more

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "simplecov"
-require "bundler/setup"
+
 require "factory_bot"
 require "remap"
 require "pry"
@@ -12,13 +12,12 @@ require_relative "examples"
 require_relative "matchers"
 
 RSpec.configure do |config|
-  config.example_status_persistence_file_path = ".rspec_status"
-  config.include Support
-  config.include Dry::Monads[:maybe, :result, :do]
   config.filter_run_when_matching :focus
-  config.order = :random
-  config.example_status_persistence_file_path = ".rspec_status"
+
+  config.include Dry::Monads[:maybe, :result, :do]
   config.include FactoryBot::Syntax::Methods
+  config.include Support
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
@@ -26,4 +25,7 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.max_formatted_output_length = nil
   end
+
+  config.example_status_persistence_file_path = ".rspec_status"
+  config.order = :random
 end
