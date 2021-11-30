bbd80f8acbf20320f7f20fe9db9549be2b395498
Linus Oleander <oleander@users.noreply.github.com>
2021-11-30 03:16:07 +0100

    Allow rules to be concerted into procs

--- lib/remap/rule.rb
+++ lib/remap/rule.rb
@@ -5,6 +5,13 @@ module Remap
     defines :requirement
     requirement Types::Any
 
+    # @param state [State]
+    #
+    # @abstract
+    def call(state)
+      raise NotImplementedError, "#{self.class}#call not implemented"
+    end
+
     # @return [Proc]
     def to_proc
       method(:call).to_proc
