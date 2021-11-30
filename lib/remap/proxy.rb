3f5fa853225f4630b5237e5a8f7dd4e93d2ea728
Linus Oleander <oleander@users.noreply.github.com>
2021-11-30 01:44:05 +0100

    explain proxy

--- lib/remap/proxy.rb
+++ lib/remap/proxy.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Remap
+  # Used by {Enum} & {Compiler} to create a clean context
   class Proxy < ActiveSupport::ProxyObject
     def self.const_missing(name)
       ::Object.const_get(name)
