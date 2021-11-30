fab324f77d69f27544aa11e5b8df098d2f69d28b
Linus Oleander <oleander@users.noreply.github.com>
2021-11-30 01:22:50 +0100

    feature spec for if_not

---
+++ spec/feature/remap/map_if_not_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+describe Remap::Base do
+  it_behaves_like described_class do
+    let(:mapper) do
+      mapper! do
+        define do
+          each do
+            map.if_not do
+              value.include?("A")
+            end
+          end
+        end
+      end
+    end
+
+    let(:input) { ["A", "B", "C"] }
+
+    let(:output) do
+      be_a_success.and(have_attributes(result: ["B", "C"]))
+    end
+  end
+end
