52459cb905afddb1d0848fb53ee36c51aceb1191
Linus Oleander <oleander@users.noreply.github.com>
2021-12-08 11:24:52 +0100

    add example

--- lib/remap/rule/map/optional.rb
+++ lib/remap/rule/map/optional.rb
@@ -9,6 +9,24 @@ module Remap
         # Represents an optional mapping rule
         # When the mapping fails, the value is ignored
         #
+        # @example Map [:name] to [:nickname]
+        #   map = Map::Optional.call({
+        #     path: {
+        #       input: [:name],
+        #       output: [:nickname]
+        #     }
+        #   })
+        #
+        #   state = Remap::State.call({
+        #     name: "John"
+        #   })
+        #
+        #   output = map.call(state) do |failure|
+        #     raise failure.exeception
+        #   end
+        #
+        #   output.fetch(:value) # => { nickname: "John" }
+        #
         # @param state [State]
         #
         # @return [State]
