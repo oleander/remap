b7feba7130c16407c7b12ec2cd2e1f64d8654d54
Linus Oleander <oleander@users.noreply.github.com>
2021-12-12 02:40:14 +0100

    new custom matcher

--- spec/feature/remap/failing_rule_spec.rb
+++ spec/feature/remap/failing_rule_spec.rb
@@ -22,11 +22,35 @@ describe Remap::Base do
     end
   end
 
-  let(:input) do
-    { age: 10 }
+  context "when age is to low" do
+    let(:input) do
+      { age: 10 }
+    end
+
+    it "invokes block with failure" do
+      expect { |e| mapper.call(input, &e) }.to yield_and_return(
+        an_instance_of(Remap::Failure).and(
+          have_attributes(
+            failures: contain_exactly(
+              an_instance_of(Remap::Notice).and(
+                have_attributes(
+                  path: [:age]
+                )
+              )
+            )
+          )
+        )
+      )
+    end
   end
 
-  it "invokes block with failure" do
-    expect { |error| mapper.call(input, &error) }.to yield_with_args(Remap::Failure)
+  context "when age higher" do
+    let(:input) do
+      { age: 100 }
+    end
+
+    it "invokes block with failure" do
+      expect { |e| mapper.call(input, &e) }.not_to yield_control
+    end
   end
 end
--- spec/matchers.rb
+++ spec/matchers.rb
@@ -21,3 +21,48 @@ RSpec::Matchers.define :contain do |expected|
     "expected #{actual.value.formatted} not to contain #{expected}"
   end
 end
+
+RSpec::Matchers.define :yield_and_return do |value|
+  callback         = proc(&:itself)
+  and_return_check = -> _ { true }
+  actual           = nil
+  and_return_value = "anything"
+  returned         = "nothing"
+
+  inner = -> v1 do
+    callback[v1].tap do |v2|
+      actual = v2
+    end
+  end
+
+  match do |probe|
+    returned = probe[inner]
+    and_return_check[returned]
+
+    values_match?(value, actual) && and_return_check[returned]
+  end
+
+  chain :with_proc do |&a_callback|
+    callback = a_callback
+  end
+
+  chain :and_return do |v1|
+    and_return_value = v1
+    and_return_check = -> v2 do
+      v1 === v2
+    end
+  end
+
+  failure_message do
+    "expected to receive %s & %s to be returned but %s were yielded & %s returned" % [
+      surface_descriptions_in(value),
+      surface_descriptions_in(and_return_value),
+      surface_descriptions_in(actual),
+      surface_descriptions_in(returned)
+    ]
+  end
+
+  def supports_block_expectations?
+    true
+  end
+end
