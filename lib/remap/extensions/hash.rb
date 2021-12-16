2ae38f8376bba3c6e8ee90c46d0c6b9161be213c
Linus Oleander <oleander@users.noreply.github.com>
2021-12-16 01:19:02 +0100

    use compact blank

--- lib/remap/extensions/hash.rb
+++ lib/remap/extensions/hash.rb
@@ -5,7 +5,7 @@ module Remap
     module Hash
       refine ::Hash do
         def formatted
-          JSON.neat_generate(self, sort: true, wrap: 40, aligned: true, around_colon: 1)
+          JSON.neat_generate(compact_blank, sort: true, wrap: 40, aligned: true, around_colon: 1)
         end
       end
     end
