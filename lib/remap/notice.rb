d571ef3ddffc0002adcaf662e015c46c072602a5
Linus Oleander <oleander@users.noreply.github.com>
2021-12-16 01:17:47 +0100

    remove unused code
    remove unused code

--- lib/remap/notice.rb
+++ lib/remap/notice.rb
@@ -20,19 +20,5 @@ module Remap
     def to_hash
       super.except(:backtrace).compact_blank
     end
-
-    # Used by State to skip mapping rules
-    #
-    # @raise [Notice::Ignore]
-    def ignore!
-      raise Ignore.new(notice: self)
-    end
-
-    # Used by the state to halt mappers
-    #
-    # @raise [Notice::Fatal]
-    def fatal!
-      raise Fatal.new(notice: self)
-    end
   end
 end
--- lib/remap/notice/error.rb
+++ lib/remap/notice/error.rb
@@ -11,27 +11,6 @@ module Remap
       delegate_missing_to :notice
       delegate :inspect, :to_s, to: :notice
       option :notice, type: Types.Instance(Notice)
-
-      def inspect
-        "#<%s %s>" % [self.class, to_hash.formatted]
-      end
-
-      def undefined(state)
-        state.set(notice: notice).except(:value)
-      end
-
-      def failure(state)
-        Failure.new(failures: [notice], notices: state.fetch(:notices))
-      end
-
-      def traced(backtrace)
-        e = Traced.new(notice: notice)
-        e.set_backtrace(backtrace)
-        e
-      end
-    end
-
-    class Traced < Error
     end
   end
 end
