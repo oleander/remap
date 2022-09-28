6fef1395a4ba74dc9d98a71f98a930463fc4c532
Linus Oleander <oleander@users.noreply.github.com>
2022-09-28 17:57:15 +0200

    allow keys to be mapped to options

--- lib/remap/path/output.rb
+++ lib/remap/path/output.rb
@@ -5,6 +5,21 @@ module Remap
     using Extensions::Enumerable
     using State::Extension
 
+    # TODO: Move & Test
+    module Dispatch
+      refine Static::Option do
+        def call(...)
+          super.value
+        end
+      end
+
+      refine Object do
+        def call(_)
+          self
+        end
+      end
+    end
+
     # Sets the value to a given path
     #
     # @example Maps "A" to { a: { b: { c: "A" } } }
@@ -13,12 +28,18 @@ module Remap
     #
     #   result.fetch(:value) # => { a: { b: { c: "A" } } }
     class Output < Unit
+      using Dispatch
+
       attribute :selectors, [Types::Key]
 
       # @return [State]
       def call(state)
+        mapped = selectors.map do |selector|
+          selector.call(state)
+        end
+
         state.fmap do |value|
-          selectors.hide(value)
+          mapped.hide(value)
         end
       end
     end
